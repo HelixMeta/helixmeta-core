66f18a6698e8409629b5b33f72edcbc7e71e1f73
Nguyen Thi Thu Thao <thao.nguyen3@sotatek.com>
2022-02-13 18:50:20 +0700

    exchange

--- test/Exchange.js
+++ test/Exchange.js
@@ -5,21 +5,38 @@ const HelixmetaExchange = jsonfile.readFileSync(
   "build/contracts/HelixmetaExchange.json"
 ).abi;
 const IERC721 = jsonfile.readFileSync("build/contracts/IERC721.json").abi;
+const IERC1155 = jsonfile.readFileSync("build/contracts/IERC1155.json").abi;
+const IWETH = jsonfile.readFileSync("build/contracts/IWETH.json").abi;
+const IHelixmetaToken = jsonfile.readFileSync(
+  "build/contracts/IHelixmetaToken.json"
+).abi;
 
-const transfererc721_manager_addr = "0x4e0D0E0e74b35bb64783F75Eba1D74b3C2034BFD"
-const helixmetaExchangeAddress = "0xf69746e7f5636d13426f41E38d6445767f682F4C";
-const erc721Address = "0x9ba7183c5b136a3e881f52ec4a8343fa1761c862";
-const MAKER_ORDER_HASH = "0x40261ade532fa1d2c7293df30aaadb9b3c616fae525a0b56d3d411c841a85028";
+//const for all deployment
+const MAKER_ORDER_HASH =
+  "0x40261ade532fa1d2c7293df30aaadb9b3c616fae525a0b56d3d411c841a85028";
+
+//replace for new deployment
+const transfererc721_manager_addr =
+  "0x4e0D0E0e74b35bb64783F75Eba1D74b3C2034BFD";
+const transfererc1155_manager_addr =
+  "0x6cc9A96AAB3e82Cd64cA6b3DAA8303049F444F98";
+const helixmeta_exchange_address = "0xf69746e7f5636d13426f41E38d6445767f682F4C";
+const helixmeta_token_address = "0x9BAf798DF6786E05A29C496d37756F1F81fCF008";
+const erc721_address = "0x9ba7183c5b136a3e881f52ec4a8343fa1761c862";
+const erc1155_address = "0x73fe1280bd329e542ebd34af1620026fe4c6d080";
 const prefix_domain =
   "0x190147d063a2b0975ac851bcfce5ad21b3feae3dfc7e0669f6f36a47619ac99ef71c";
+const WETH = process.env.WETH;
+const standard_strategy = "0x5aAA091990edc6657a20605974a6FE9048F7F342";
+
+//replace for each test (increate 1 for token, 3 for nonce)
+const token_id_erc = 12;
+const nonce = 11;
 
 const SK_acc0 =
   "d278b1141f204160fcbed9ea0b3c33b74a4f0a2207ec4482b77ffabde8c0c35a";
 const SK_acc1 =
   "c31846bff0b9e65a3de758d77483ea0998ecd0c7b74b631e640e0f639c326b38";
-
-const WETH = process.env.WETH;
-const standard_strategy = "0x5aAA091990edc6657a20605974a6FE9048F7F342";
 
 function hash_(makerOrder) {
   const encode =
@@ -40,13 +57,13 @@ function hash_(makerOrder) {
       .padStart(64, "0") +
     web3.utils.sha3(makerOrder.params).slice(2);
 
-  console.log("encode: ", encode);
+  // console.log("encode: ", encode);
   const maker_order_hash = web3.utils.sha3(encode);
-  console.log("make_order_hash", maker_order_hash);
+  // console.log("make_order_hash", maker_order_hash);
   const prefix_hash = web3.utils.sha3(
     prefix_domain + maker_order_hash.slice(2)
   );
-  console.log("prefix_domain", prefix_domain);
+  // console.log("prefix_domain", prefix_domain);
   return prefix_hash.slice(2);
 }
 
@@ -60,27 +77,37 @@ function sign_hash(inputHash, pkeyConfig) {
   (signature.r = ethUtil.bufferToHex(signature_getting.r)),
     (signature.s = ethUtil.bufferToHex(signature_getting.s)),
     (signature.v = ethUtil.bufferToHex(signature_getting.v));
-  console.log(signature);
+  // console.log(signature);
   return signature;
 }
 
-contract("match ask with taker bid using ETH and WETH", function () {
-  it("should approve NFT721 tokenID 1 by maker ask from acc0 to HelixmetaExchange", async function () {
+contract("exchange for users", function () {
+  it("should match ask with taker bid using ETH and WETH", async function () {
     const accounts = await web3.eth.getAccounts();
-    // load exchange contract
-    const ERC721 = new web3.eth.Contract(IERC721, erc721Address);
-    const result = await ERC721.methods.approve(transfererc721_manager_addr, 5).send({
-      from: accounts[0],
-    })
-    console.log(result)
-  });
-  it("should taker bid using ETH", async function () {
-    const accounts = await web3.eth.getAccounts();
+
+    //  Account taker bid approve weth for helixmeta exchange
+    // load weth contract
+    const weth = new web3.eth.Contract(IWETH, process.env.WETH);
+    await weth.methods
+      .approve(helixmeta_exchange_address, "1000000000000000")
+      .send({
+        from: accounts[1],
+      });
+
+    //  Account ask approve erc721 for transfer manager
+    // load erc721 contract
+    const ERC721 = new web3.eth.Contract(IERC721, erc721_address);
+    await ERC721.methods
+      .approve(transfererc721_manager_addr, token_id_erc)
+      .send({
+        from: accounts[0],
+      });
+
     var takerBid = {
       isOrderAsk: false,
       taker: accounts[1],
       price: "1000000000000000",
-      tokenId: 5,
+      tokenId: token_id_erc,
       minPercentageToAsk: 7000,
       params:
         "0x40261ade532fa1d2c7293df30aaadb9b3c616fae525a0b56d3d411c841a85028000000000000000000000000000000000000000000000000000000000000000100000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c300000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c30000000000000000000000000000000000000000000000000429d069189e000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000e4a1009bd1945812fc6f203b9196cd7c57299c1800000000000000000000000089c24cc1cb5ee3c6f1e786b52f754230bd20a7f9",
@@ -89,15 +116,15 @@ contract("match ask with taker bid using ETH and WETH", function () {
     var makerAsk = {
       isOrderAsk: true,
       signer: accounts[0], // signer of the maker order
-      collection: erc721Address, // collection address
+      collection: erc721_address, // collection address
       price: "1000000000000000", // price (used as )
-      tokenId: 5, // id of the token
+      tokenId: token_id_erc, // id of the token
       amount: 1, // amount of tokens to sell/purchase (must be 1 for ERC721, 1+ for ERC1155)
       strategy: standard_strategy, // strategy for trade execution (e.g., DutchAuction, StandardSaleForFixedPrice)
       currency: WETH, // currency (e.g., WETH)
-      nonce: 1, // order nonce (must be unique unless new maker order is meant to override existing one e.g., lower ask price)
-      startTime: 1643881562, // startTime in timestamp
-      endTime: 1654251566, // endTime in timestamp
+      nonce: nonce, // order nonce (must be unique unless new maker order is meant to override existing one e.g., lower ask price)
+      startTime: Math.floor(Date.now() / 1000) - 2000, // startTime in timestamp
+      endTime: Math.floor(Date.now() / 1000) + 5000, // endTime in timestamp
       minPercentageToAsk: 7000, // slippage protection (9000 --> 90% of the final price must return to ask)
       params:
         "0x40261ade532fa1d2c7293df30aaadb9b3c616fae525a0b56d3d411c841a85028000000000000000000000000000000000000000000000000000000000000000100000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c300000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c30000000000000000000000000000000000000000000000000429d069189e000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000e4a1009bd1945812fc6f203b9196cd7c57299c1800000000000000000000000089c24cc1cb5ee3c6f1e786b52f754230bd20a7f9", // additional parameters
@@ -116,7 +143,7 @@ contract("match ask with taker bid using ETH and WETH", function () {
     //  load exchange contract
     const helixmetaExchange = new web3.eth.Contract(
       HelixmetaExchange,
-      helixmetaExchangeAddress
+      helixmeta_exchange_address
     );
 
     const result = await helixmetaExchange.methods
@@ -124,6 +151,188 @@ contract("match ask with taker bid using ETH and WETH", function () {
       .send({
         from: accounts[1],
       });
-    console.log(result);
+    // console.log(result);
   });
+
+  it("should match ask with taker bid using helixmeta", async function () {
+    const accounts = await web3.eth.getAccounts();
+
+    // load hlmtoken contract
+    const hlmToken = new web3.eth.Contract(
+      IHelixmetaToken,
+      helixmeta_token_address
+    );
+    await hlmToken.methods
+      .approve(helixmeta_exchange_address, "1000000000000000")
+      .send({
+        from: accounts[0],
+      });
+
+    // load erc1155 contract
+    const ERC1155 = new web3.eth.Contract(IERC1155, erc1155_address);
+    await ERC1155.methods
+      .setApprovalForAll(transfererc1155_manager_addr, true)
+      .send({
+        from: accounts[1],
+      });
+
+    var takerBid = {
+      isOrderAsk: false,
+      taker: accounts[0],
+      price: "1000000000000000",
+      tokenId: token_id_erc,
+      minPercentageToAsk: 7000,
+      params:
+        "0x40261ade532fa1d2c7293df30aaadb9b3c616fae525a0b56d3d411c841a85028000000000000000000000000000000000000000000000000000000000000000100000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c300000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c30000000000000000000000000000000000000000000000000429d069189e000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000e4a1009bd1945812fc6f203b9196cd7c57299c1800000000000000000000000089c24cc1cb5ee3c6f1e786b52f754230bd20a7f9",
+    };
+
+    var makerAsk = {
+      isOrderAsk: true,
+      signer: accounts[1], // signer of the maker order
+      collection: erc1155_address, // collection address
+      price: "1000000000000000", // price (used as )
+      tokenId: token_id_erc, // id of the token
+      amount: 2, // amount of tokens to sell/purchase (must be 1 for ERC721, 1+ for ERC1155)
+      strategy: standard_strategy, // strategy for trade execution (e.g., DutchAuction, StandardSaleForFixedPrice)
+      currency: helixmeta_token_address, // currency (e.g., WETH)
+      nonce: nonce, // order nonce (must be unique unless new maker order is meant to override existing one e.g., lower ask price)
+      startTime: Math.floor(Date.now() / 1000) - 2000, // startTime in timestamp
+      endTime: Math.floor(Date.now() / 1000) + 5000, // endTime in timestamp
+      minPercentageToAsk: 7000, // slippage protection (9000 --> 90% of the final price must return to ask)
+      params:
+        "0x40261ade532fa1d2c7293df30aaadb9b3c616fae525a0b56d3d411c841a85028000000000000000000000000000000000000000000000000000000000000000100000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c300000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c30000000000000000000000000000000000000000000000000429d069189e000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000e4a1009bd1945812fc6f203b9196cd7c57299c1800000000000000000000000089c24cc1cb5ee3c6f1e786b52f754230bd20a7f9", // additional parameters
+      v: "", // v: parameter (27 or 28)
+      r: "", // r: parameter
+      s: "", // s: parameter
+    };
+
+    const hash_data = hash_(makerAsk);
+    const sign_data = sign_hash(hash_data, SK_acc1);
+    // console.log(sign_data);
+    makerAsk.v = sign_data.v;
+    makerAsk.r = sign_data.r;
+    makerAsk.s = sign_data.s;
+
+    //  load exchange contract
+    const helixmetaExchange = new web3.eth.Contract(
+      HelixmetaExchange,
+      helixmeta_exchange_address
+    );
+
+    const result = await helixmetaExchange.methods
+      .matchAskWithTakerBid(takerBid, makerAsk)
+      .send({
+        from: accounts[0],
+      });
+    // console.log(result);
+  });
+
+  it("should match bid with taker ask using helixmeta or weth", async function () {
+    const accounts = await web3.eth.getAccounts();
+
+    // load hlmtoken contract
+    const hlmToken = new web3.eth.Contract(
+      IHelixmetaToken,
+      helixmeta_token_address
+    );
+    await hlmToken.methods
+      .approve(helixmeta_exchange_address, "1000000000000000")
+      .send({
+        from: accounts[0],
+      });
+
+    // load erc721 contract
+    const ERC721 = new web3.eth.Contract(IERC721, erc721_address);
+    await ERC721.methods
+      .approve(transfererc721_manager_addr, token_id_erc)
+      .send({
+        from: accounts[1],
+      });
+
+    var takerAsk = {
+      isOrderAsk: true,
+      taker: accounts[1],
+      price: "1000000000000000",
+      tokenId: token_id_erc,
+      minPercentageToAsk: 7000,
+      params:
+        "0x40261ade532fa1d2c7293df30aaadb9b3c616fae525a0b56d3d411c841a85028000000000000000000000000000000000000000000000000000000000000000100000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c300000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c30000000000000000000000000000000000000000000000000429d069189e000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000e4a1009bd1945812fc6f203b9196cd7c57299c1800000000000000000000000089c24cc1cb5ee3c6f1e786b52f754230bd20a7f9",
+    };
+
+    var makerBid = {
+      isOrderAsk: false,
+      signer: accounts[0], // signer of the maker order
+      collection: erc721_address, // collection address
+      price: "1000000000000000", // price (used as )
+      tokenId: token_id_erc, // id of the token
+      amount: 1, // amount of tokens to sell/purchase (must be 1 for ERC721, 1+ for ERC1155)
+      strategy: standard_strategy, // strategy for trade execution (e.g., DutchAuction, StandardSaleForFixedPrice)
+      currency: helixmeta_token_address, // currency (e.g., WETH)
+      nonce: nonce + 1, // order nonce (must be unique unless new maker order is meant to override existing one e.g., lower ask price)
+      startTime: Math.floor(Date.now() / 1000) - 2000, // startTime in timestamp
+      endTime: Math.floor(Date.now() / 1000) + 5000, // endTime in timestamp
+      minPercentageToAsk: 7000, // slippage protection (9000 --> 90% of the final price must return to ask)
+      params:
+        "0x40261ade532fa1d2c7293df30aaadb9b3c616fae525a0b56d3d411c841a85028000000000000000000000000000000000000000000000000000000000000000100000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c300000000000000000000000049114597ef077b8ddfa8c2be2dd35a1fe5c586c30000000000000000000000000000000000000000000000000429d069189e000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000e4a1009bd1945812fc6f203b9196cd7c57299c1800000000000000000000000089c24cc1cb5ee3c6f1e786b52f754230bd20a7f9", // additional parameters
+      v: "", // v: parameter (27 or 28)
+      r: "", // r: parameter
+      s: "", // s: parameter
+    };
+
+    const hash_data = hash_(makerBid);
+    const sign_data = sign_hash(hash_data, SK_acc0);
+    // console.log(sign_data);
+    makerBid.v = sign_data.v;
+    makerBid.r = sign_data.r;
+    makerBid.s = sign_data.s;
+
+    //  load exchange contract
+    const helixmetaExchange = new web3.eth.Contract(
+      HelixmetaExchange,
+      helixmeta_exchange_address
+    );
+
+    const result = await helixmetaExchange.methods
+      .matchBidWithTakerAsk(takerAsk, makerBid)
+      .send({
+        from: accounts[1],
+      });
+    // console.log(result);
+  });
+
+  it("should cancel all orders for sender", async function () {
+    const accounts = await web3.eth.getAccounts();
+
+     //  load exchange contract
+     const helixmetaExchange = new web3.eth.Contract(
+      HelixmetaExchange,
+      helixmeta_exchange_address
+    );
+
+    const result = await helixmetaExchange.methods
+      .cancelAllOrdersForSender(nonce + 2)
+      .send({
+        from: accounts[0],
+      });
+    // console.log(result);
+  })
+
+  it("should cancel multiple maker order", async function () {
+    const accounts = await web3.eth.getAccounts();
+
+     //  load exchange contract
+     const helixmetaExchange = new web3.eth.Contract(
+      HelixmetaExchange,
+      helixmeta_exchange_address
+    );
+
+    const result = await helixmetaExchange.methods
+      .cancelMultipleMakerOrders([nonce+1, nonce+2])
+      .send({
+        from: accounts[1],
+      });
+    // console.log(result);
+  })
 });
+
+
---
+++ test/trading_reward.js
@@ -0,0 +1,56 @@
+const jsonfile = require("jsonfile");
+
+const TradingRewardsDistributor = jsonfile.readFileSync(
+  "build/contracts/TradingRewardsDistributor.json"
+).abi;
+
+const trading_reward_address = "0x13233ab1643Dc340781943A62C64e16DC9eE9F8B";
+
+function hash_acc_and_amount(acc_addr, amount) {
+  return web3.utils.sha3(
+    "0x" +
+      acc_addr.toLowerCase().slice(2) +
+      BigInt(amount.toString()).toString(16).padStart(64, "0")
+  );
+}
+
+function hash_hash(hash1, hash2) {
+  if (hash1 >= hash2) return web3.utils.sha3(hash2 + hash1.slice(2));
+  else return web3.utils.sha3(hash1 + hash2.slice(2));
+}
+
+contract("trading reward", function () {
+  // Assign trading nft account and amount
+  const acc = [
+    "0x256628a73776058Da1673dFC0315039e40DD30Eb",
+    "0x1bb21FE614cA37A5FA13c2BAAb42465C2E574a46",
+    "0xf3e75D0643CAD65B50F54EDa4253F7206692536e",
+  ];
+  const amount = ["1000000000000000", "2430000000000000", "10000000000000"];
+  var hash_acc_amount = [];
+  for (var i = 1; i < hash_acc_amount.length; i++) {
+    hash_acc_amount[i] = hash_acc_and_amount(acc[i], amount[i]);
+  }
+  var root = hash_acc_amount[0];
+  for (var i = 1; i < hash_acc_amount.length; i++) {
+    root = hash_hash(root, hash_acc_amount[i]);
+  }
+  console.log(root)
+  it("should update trading rewards", async function () {
+    const accounts = await web3.eth.getAccounts();
+
+    //  load exchange contract
+    const trading_reward_instance = new web3.eth.Contract(
+      TradingRewardsDistributor,
+      trading_reward_address
+    );
+
+    const result = await trading_reward_instance.methods
+      .updateTradingRewards(root, "1000000000000000")
+      .send({
+        from: accounts[1],
+      });
+    console.log(result);
+  });
+
+});
